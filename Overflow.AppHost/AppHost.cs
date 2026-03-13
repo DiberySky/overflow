298590187a685b16288c6898f2f8f520ddb5c692
DiberySky <7848176@qq.com>
2026-03-13 08:32:02 +0800

    aspire 13.1

--- Overflow.AppHost/AppHost.cs
+++ Overflow.AppHost/AppHost.cs
@@ -1,17 +1,36 @@
+using System.Runtime.CompilerServices;
+using System.Xml.Serialization;
+using Microsoft.Extensions.Hosting;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
+var compose = builder.AddDockerComposeEnvironment("production")
+    .WithDashboard(dashboard => dashboard.WithHostPort(8080));
+
 var keycloak = builder.AddKeycloak("keycloak", 6001)
-    .WithDataVolume("keycloak-data-volume");
+    .WithDataVolume("keycloak-data-volume")
+    .WithEnvironment("KC_HTTP_ENABLED", "true")
+    .WithEnvironment("KC_HOSTNAME_STRICT", "false");
+    // .WithEndpoint(6001, 8080, "keycloak", isExternal: true);
+
+// var identity = builder.AddKeycloak("idp", 8443)
+//     .WithArgs(["--http-enabled", "true"])
+//     .WithLifetime(ContainerLifetime.Persistent);
 
 var postgres = builder.AddPostgres("postgres", port:5432)
     .WithDataVolume("postgres-data-volume")
     .WithPgAdmin();
 
-var typesenseApiKey = builder.AddParameter("typesense-api-key", secret: true);
+// var typesenseApiKey = builder.AddParameter("typesense-api-key", secret: true);
+var typesenseApiKey = builder.Environment.IsDevelopment()
+    ? builder.Configuration["Parameters:typesense-api-key"] 
+      ?? throw new InvalidOperationException("Could not get typesense api key")
+    : "${TYPESENSE_API_KEY} ";
 
 var typesense = builder.AddContainer("typesense", "typesense/typesense", "30.1")
     .WithArgs("--data-dir", "/data", "--api-key", typesenseApiKey, "--enable-cors")
     .WithVolume("typesense-data-volume", "/data")
+    .WithEnvironment("TYPESENSE_API_KEY", typesenseApiKey)
     .WithHttpEndpoint(8108, 8108, name: "typesense");
 
 var typesenseContainer = typesense.GetEndpoint("typesense");
@@ -37,5 +56,16 @@ var searchService = builder.AddProject<Projects.SearchService>("search-svc")
     .WaitFor(keycloak)
     .WaitFor(typesense)
     .WaitFor(rabbitmq);
+
+var yarp = builder.AddYarp("gateway")
+    .WithConfiguration(yarpBuilder =>
+    {
+        yarpBuilder.AddRoute("/questions/{**catch-all}", questionService);
+        yarpBuilder.AddRoute("/tags/{**catch-all}", questionService);
+        yarpBuilder.AddRoute("/search/{**catch-all}", searchService);
+    })
+    .WithEnvironment("ASPNETCORE_URLS", "http://*:8001")
+    .WithEndpoint(port: 8001, targetPort:8001, scheme:"http", name: "gateway", isExternal: true);
+
 
 builder.Build().Run();
--- QuestionService/Controllers/QuestionsController.cs
+++ QuestionService/Controllers/QuestionsController.cs
@@ -197,7 +197,7 @@ public class QuestionsController(QuestionDbContext db, IMessageBus bus, TagServi
         
         await db.SaveChangesAsync();
         
-        bus.PublishAsync(new AnswerCountUpdated(question.Id, question.AnswerCount));
+        await bus.PublishAsync(new AnswerCountUpdated(question.Id, question.AnswerCount));
         
         return NoContent();
     }
@@ -220,5 +220,22 @@ public class QuestionsController(QuestionDbContext db, IMessageBus bus, TagServi
         await bus.PublishAsync(new AnswerAccepted(questionId));
 
         return NoContent();
+    }
+
+    [HttpGet("errors")]
+    public ActionResult GetError(int code)
+    {
+        ModelState.AddModelError("Problem one", "Validation problem one");
+        ModelState.AddModelError("Problem two", "Validation problem two");
+
+        return code switch
+        {
+            400 => BadRequest("Opposite of good request"),
+            401 => Unauthorized(),
+            403 => Forbid(),
+            404 => NotFound(),
+            500 => throw new Exception("This is a server error"),
+            _ => ValidationProblem(ModelState)
+        };
     }
 }
